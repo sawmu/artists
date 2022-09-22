87ad05f61beae2a1aca8308311569a3ec7fd58fc
Saw <saw@firstcom.com.sg>
2022-09-22 16:39:18 +0800

    update/banner

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from 'react';
+import BannerImage from '../public/banner.jpg';
+import Image from 'next/image';
 
 type Props = {}
 
 function Hero({}: Props) {
   return (
-    <div>Hero</div>
+    <div className='hero-backgorund  '>
+        <div className="container">
+            <div className='h-screen flex justify-center items-center text-center'>
+                <div className="flex flex-wrap items-center  text-center">
+                    <div className=" flex flex-col justify-end">
+                        <h1 className="font-bold mb-2 text-white text-4xl md:text-5xl">FEATURED ARTISTS</h1> 
+                        <p className='text-sm px-10'>Adam Blackstone and Brian Frasier-Moore discuss Super Bowl 2020 and more.</p>
+                        <div className='py-5'>
+                            <a href="#" className="text-sm text-bold uppercase py-3 px-8 bg-[rgb(243,121,32)] hover:bg-[rgb(245,135,57)] text-white rounded-sm hover:shadow-xl transition duration-300">Read More</a>
+                        </div>  
+                    </div>
+                </div>  
+            </div>
+        </div>
+        
+
+    </div>
+
+
+ 
+    
   )
 }
+
+
 
 export default Hero
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,17 +6,16 @@ import Hero from '../components/Hero'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <div className='bg-[rgb(10,10,10)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0'>
       <Head>
         <title>Artists</title>
         <meta name="description" content="Artists Description" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
    
-      <Header />
+      {/* <Header /> */}
 
-      {/* Hero */}
-      <section id='hero'>
+      <section id='hero' className='snap-center'>
         <Hero />
       </section>
 
